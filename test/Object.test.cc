6add08b103bed080543a46227f1c07a754af5fdb
M. Shulhan <ms@kilabit.info>
2017-06-05 01:16:22 +0700

    [test] Object: use Test module

--- test/Object.test.cc
+++ test/Object.test.cc
@@ -6,17 +6,21 @@
 
 #include <assert.h>
 
+#include "test.hh"
 #include "../Object.hh"
+#include "../Test.hh"
 
 using vos::Object;
+using vos::Test;
+
+Test T(Object::__CNAME);
 
 int main()
 {
 	Object o;
 
-	assert(strcmp(o.__CNAME, "Object") == 0);
-	assert(o.__str == NULL);
-	assert(o.chars() == NULL);
+	T.expect_string(o.__CNAME, "Object", 0);
+	T.expect_ptr(o.chars(), NULL, 0);
 }
 
-// vi: ts=8 sw=8 tw=78:
+// vi: ts=8 sw=8 tw=80:
