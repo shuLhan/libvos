eea880538d1aa02fe97c0a8d6278316402dddd5e
M. Shulhan <ms@kilabit.info>
2017-06-07 01:37:31 +0700

    Error.SYS(): new method to convert stdlib error string to Error
    Also,
    
    * Add pointer to data to minimize creating data when copying Error object
    * Add methods to get data as value by type

--- Error.cc
+++ Error.cc
@@ -8,14 +8,39 @@
 
 namespace vos {
 
-const Error ErrOutOfMemory("libvos: Out of memory");
 const Error ErrNumRange("libvos: Numerical result out of range");
+const Error ErrOutOfMemory("libvos: Out of memory");
+Error ErrUnknown("libvos: unknown error");
 
 const char* Error::__CNAME = "Error";
+
+Error Error::SYS()
+{
+	int s;
+	int errnum = errno;
+	size_t len = 512;
+	char* msg = (char*) calloc(len, 1);
+
+	do {
+		s = strerror_r(errnum, msg, len);
+		if (s && errno == ERANGE) {
+			len = len * 2;
+			msg = (char*) realloc(msg, len);
+		}
+	} while (errno == ERANGE);
+
+	if (errno == EINVAL) {
+		return ErrUnknown.with(&errnum, sizeof(int));
+	}
+
+	return Error(msg);
+}
 
 Error::Error()
 : Object()
 , _data(NULL)
+, _p_data(NULL)
+, _len(0)
 {
 	__str = NULL;
 }
@@ -23,31 +48,52 @@ Error::Error()
 Error::Error(const char* msg)
 : Object()
 , _data(NULL)
+, _p_data(NULL)
+, _len(0)
 {
 	__str = (char*) msg;
 }
 
+/**
+ * Method Error(err) will create new error by referencing error message and
+ * copying error data.
+ */
 Error::Error(const Error& err)
 : Object()
 , _data(NULL)
+, _p_data(NULL)
+, _len(0)
 {
 	__str = err.__str;
-	_data = err._data;
+	if (err._data) {
+		_p_data = err._data;
+	}
+	if (err._p_data) {
+		_p_data = err._p_data;
+	}
+	_len = err._len;
 }
 
+/**
+ * Method ~Error() will release error object from memory.
+ */
 Error::~Error()
 {
 	__str = NULL;
-	if (_data) {
+	if (_data != NULL) {
 		free(_data);
 		_data = NULL;
 	}
+	_p_data = NULL;
+	_len = 0;
 }
 
 Error& Error::operator=(const Error& err)
 {
-	this->__str = err.__str;
-	this->_data = err._data;
+	__str = err.__str;
+	_data = err._data;
+	_p_data = err._p_data;
+	_len = err._len;
 	return *this;
 }
 
@@ -61,21 +107,121 @@ int Error::operator!=(const Error& err) const
 	return (__str != err.__str);
 }
 
-Error& Error::With(const void* data, size_t len)
+Error Error::with(const void* data, size_t len)
 {
-	if (_data) {
-		free(_data);
-	}
+	Error err(__str);
+
 	if (data) {
-		_data = calloc(len, 1);
-		memcpy(_data, data, len);
+		err._data = calloc(len, 1);
+		memcpy(err._data, data, len);
+		err._len = len;
 	}
-	return *this;
+
+	return err;
 }
 
-void* Error::Data()
+void* Error::data() const
 {
+	if (_p_data) {
+		return _p_data;
+	}
 	return _data;
+}
+
+char Error::data_as_char() const
+{
+	if (_len == 0) {
+		return 0;
+	}
+
+	const char* str;
+
+	if (_p_data) {
+		str = (const char*) _p_data;
+	} else {
+		str = (const char*) _data;
+	}
+
+	return str[0];
+}
+
+const char* Error::data_as_string() const
+{
+	if (_len == 0) {
+		return 0;
+	}
+	if (_p_data) {
+		return (const char*) _p_data;
+	}
+	return (const char*) _data;
+}
+
+long int Error::data_as_signed() const
+{
+	long int data = 0;
+
+	if (_len == 0) {
+		return 0;
+	}
+
+	switch (_len) {
+	case sizeof(int):
+		if (_p_data) {
+			data = *(int*) _p_data;
+		} else {
+			data = *(int*) _data;
+		}
+		break;
+	case sizeof(long int):
+		if (_p_data) {
+			data = *(long int*) _p_data;
+		} else {
+			data = *(long int*) _data;
+		}
+		break;
+	}
+
+	return data;
+}
+
+unsigned long Error::data_as_unsigned() const
+{
+	unsigned long data = 0;
+
+	if (_len == 0) {
+		return 0;
+	}
+
+	switch (_len) {
+	case sizeof(unsigned int):
+		if (_p_data) {
+			data =  *(unsigned int *) _p_data;
+		} else {
+			data =  *(unsigned int *) _data;
+		}
+		break;
+	case sizeof(unsigned long):
+		if (_p_data) {
+			data =  *(unsigned long *) _p_data;
+		} else {
+			data =  *(unsigned long *) _data;
+		}
+		break;
+	}
+
+	return data;
+}
+
+double Error::data_as_double() const
+{
+	if (_len == 0) {
+		return 0;
+	}
+
+	if (_p_data) {
+		return *(double*) _p_data;
+	}
+	return *(double*) _data;
 }
 
 } // namespace vos
--- Error.hh
+++ Error.hh
@@ -11,9 +11,6 @@
 
 namespace vos {
 
-extern const Error ErrOutOfMemory;
-extern const Error ErrNumRange;
-
 /**
  * Class Error represent error type as object with string and data.
  *
@@ -22,6 +19,8 @@ extern const Error ErrNumRange;
 class Error : public Object {
 public:
 	static const char* __CNAME;
+
+	static Error SYS();
 
 	Error();
 	Error(const char* msg);
@@ -33,12 +32,24 @@ public:
 	int operator==(const Error& err) const;
 	int operator!=(const Error& err) const;
 
-	Error& With(const void* data, size_t len);
-	void* Data();
+	Error with(const void* data, size_t len);
+
+	void* data() const;
+	char data_as_char() const;
+	const char* data_as_string() const;
+	long int data_as_signed() const;
+	unsigned long data_as_unsigned() const;
+	double data_as_double() const;
 
 private:
 	void* _data;
+	void* _p_data;
+	size_t _len;
 };
+
+extern const Error ErrNumRange;
+extern const Error ErrOutOfMemory;
+extern Error ErrUnknown;
 
 } // namespace vos
 #endif // _LIBVOS_EROR_HH
