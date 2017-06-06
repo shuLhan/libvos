0349bb2f33fe43f01664e9ffc2f8d217ffd4b915
M. Shulhan <ms@kilabit.info>
2017-06-07 01:13:30 +0700

    test/test.hh: follow coding style, remove unused variable and declarations

--- test/test.cc
+++ test/test.cc
@@ -1,12 +1,10 @@
 //
-// Copyright 2009-2016 M. Shulhan (ms@kilabit.info). All rights reserved.
+// Copyright 2009-2017 M. Shulhan (ms@kilabit.info). All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 //
 
 #include "test.hh"
-
-Dlogger D;
 
 int expectString(const char* exp, const char* got, int to)
 {
@@ -21,3 +19,5 @@ int expectString(const char* exp, const char* got, int to)
 
 	exit(1);
 }
+
+// vi: ts=8 sw=8 tw=80:
--- test/test.hh
+++ test/test.hh
@@ -1,17 +1,15 @@
 //
-// Copyright 2009-2016 M. Shulhan (ms@kilabit.info). All rights reserved.
+// Copyright 2009-2017 M. Shulhan (ms@kilabit.info). All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 //
 
 #include <assert.h>
 
-#include "../Buffer.hh"
-#include "../Dlogger.hh"
+#include "../Test.hh"
 
+using vos::Test;
 using vos::Error;
-using vos::Buffer;
-using vos::Dlogger;
 
 #define CB(s)		"{ " s " }"
 #define SB(s)		"[ " s " ]"
@@ -27,7 +25,6 @@ using vos::Dlogger;
 #define STR_TEST_1	"test 1"
 #define STR_TEST_2	"test 2"
 
-extern Dlogger D;
 extern int expectString(const char* exp, const char* got, int to);
 
-// vi: ts=8 sw=8 tw=78:
+// vi: ts=8 sw=8 tw=80:
