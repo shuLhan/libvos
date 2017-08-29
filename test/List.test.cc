273029b903a8ccc021fadb101def4a9646eb527f
M. Shulhan <ms@kilabit.info>
2017-08-30 01:44:12 +0700

    test/List: replace assert with module Test

--- test/List.test.cc
+++ test/List.test.cc
@@ -10,6 +10,9 @@
 using vos::Object;
 using vos::List;
 using vos::BNode;
+using vos::Test;
+
+Test T("List");
 
 #define EXP_0		SB(V_STR(STR_TEST_0))
 #define EXP_1		SB(V_STR(STR_TEST_1))
@@ -53,129 +56,129 @@ int s;
 Object* item = NULL;
 Buffer* b = NULL;
 
-Buffer* test_push_tail(const char *str, int exp_n, const char *exp_chars)
+Buffer* test_push_tail(const char* str, int exp_n, const char* exp_chars)
 {
 	b = new Buffer();
 	b->copy_raw(str);
 
 	list.push_tail(b);
-	assert(list.size() == exp_n);
+	T.expect_signed(list.size(), exp_n);
 
-	assert(strcmp(exp_chars, list.chars()) == 0);
+	T.expect_string(exp_chars, list.chars());
 
 	return b;
 }
 
-void test_push_head(const char *str, int exp_n, const char *exp_chars)
+void test_push_head(const char* str, int exp_n, const char* exp_chars)
 {
 	b = new Buffer();
 	b->copy_raw(str);
 
 	list.push_head(b);
-	assert(list.size() == exp_n);
+	T.expect_signed(list.size(), exp_n);
 
-	assert(strcmp(exp_chars, list.chars()) == 0);
+	T.expect_string(exp_chars, list.chars());
 }
 
 void test_at_0()
 {
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	item = list.at(0);
-	assert(item == NULL);
+	T.expect_ptr(item, NULL);
 
 	item = list.at(1);
-	assert(item == NULL);
+	T.expect_ptr(item, NULL);
 }
 
 void test_at_1(const char* exp)
 {
-	assert(list.size() == 1);
+	T.expect_signed(list.size(), 1);
 
 	// REMEMBER: circular!
 	item = list.at(-1);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp);
 
 	item = list.at(0);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp);
 
 	// REMEMBER: circular!
 	item = list.at(1);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp);
 }
 
 void test_at_2(const char* exp1, const char* exp2)
 {
-	assert(list.size() == 2);
+	T.expect_signed(list.size(), 2);
 
 	item = list.at(-2);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp1) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp1);
 
 	item = list.at(-1);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp2) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp2);
 
 	item = list.at(0);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp1) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp1);
 
 	item = list.at(1);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp2) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp2);
 
 	item = list.at(2);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp1) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp1);
 }
 
 void test_at_3(const char* exp0, const char* exp1, const char* exp2)
 {
-	assert(list.size() == 3);
+	T.expect_signed(list.size(), 3);
 
 	item = list.at(-3);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp0) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp0);
 
 	item = list.at(-2);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp1) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp1);
 
 	item = list.at(-1);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp2) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp2);
 
 	item = list.at(0);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp0) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp0);
 
 	item = list.at(1);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp1) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp1);
 
 	item = list.at(2);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp2) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp2);
 
 	item = list.at(3);
-	assert(item != NULL);
-	assert(strcmp(item->chars(), exp0) == 0);
+	T.expect_ptr(item, NULL, vos::IS_NOT_EQUAL);
+	T.expect_string(item->chars(), exp0);
 }
 
 void test_pop_tail(const char* exp_b, int exp_n, const char* exp_chars)
 {
 	b = (Buffer*) list.pop_tail();
 
-	assert(list.size() == exp_n);
+	T.expect_signed(list.size(), exp_n);
 
 	if (exp_chars) {
-		assert(strcmp(exp_chars, list.chars()) == 0);
+		T.expect_string(exp_chars, list.chars());
 	}
 
-	assert(strcmp(exp_b, b->chars()) == 0);
+	T.expect_string(exp_b, b->chars());
 	delete b;
 }
 
@@ -183,13 +186,13 @@ void test_pop_head(const char* exp_b, int exp_n, const char* exp_chars)
 {
 	b = (Buffer*) list.pop_head();
 
-	assert(list.size() == exp_n);
+	T.expect_signed(list.size(), exp_n);
 
 	if (exp_chars) {
-		assert(strcmp(exp_chars, list.chars()) == 0);
+		T.expect_string(exp_chars, list.chars());
 	}
 
-	assert(strcmp(exp_b, b->chars()) == 0);
+	T.expect_string(exp_b, b->chars());
 	delete b;
 }
 
@@ -206,86 +209,86 @@ void test_remove()
 
 	// (C0) Item not found, parameter is empty.
 	s = list.remove(NULL);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 
 	// (C1) Item not found, list is empty.
 	s = list.remove(b0);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 
 	// (C1) Item not found, when searching different item.
 	list.push_tail(b0);
-	assert(list.size() == 1);
+	T.expect_signed(list.size(), 1);
 
 	s = list.remove(b1);
-	assert(s == 1);
-	assert(list.size() == 1);
+	T.expect_signed(s, 1);
+	T.expect_signed(list.size(), 1);
 
 	// (C2) Item found, but its the only item in the list
 	s = list.remove(b0);
-	assert(s == 0);
-	assert(list.size() == 0);
+	T.expect_signed(s, 0);
+	T.expect_signed(list.size(), 0);
 
 	// (C1) Item not found, because its already removed.
 	s = list.remove(b0);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 
 	// (C3) Item found, and its also the head
 	list.push_tail(b0);
 	list.push_tail(b1);
 	list.push_tail(b2);
-	assert(list.size() == 3);
+	T.expect_signed(list.size(), 3);
 
 	s = list.remove(b0);
-	assert(s == 0);
-	assert(list.size() == 2);
-	assert(strcmp(list.chars(), EXP_0_0) == 0);
+	T.expect_signed(s, 0);
+	T.expect_signed(list.size(), 2);
+	T.expect_string(list.chars(), EXP_0_0);
 
 	// (C1) Item not found
 	s = list.remove(b0);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 
 	// (C4) Item found, and its also the tail
 	s = list.remove(b2);
-	assert(s == 0);
-	assert(list.size() == 1);
-	assert(strcmp(list.chars(), EXP_0) == 0);
+	T.expect_signed(s, 0);
+	T.expect_signed(list.size(), 1);
+	T.expect_string(list.chars(), EXP_0);
 
 	// (C1) Item not found
 	s = list.remove(b0);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 	s = list.remove(b2);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 
 	// (C2) Item found, but its the only item in the list
 	s = list.remove(b1);
-	assert(s == 0);
-	assert(list.size() == 0);
+	T.expect_signed(s, 0);
+	T.expect_signed(list.size(), 0);
 
 	// (C1) Item not found
 	s = list.remove(b0);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 	s = list.remove(b1);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 	s = list.remove(b2);
-	assert(s == 1);
+	T.expect_signed(s, 1);
 
 	// (C5) Item found, and its in the middle
 	list.push_tail(b0);
 	list.push_tail(b1);
 	list.push_tail(b2);
-	assert(list.size() == 3);
+	T.expect_signed(list.size(), 3);
 
 	s = list.remove(b1);
-	assert(s == 0);
-	assert(list.size() == 2);
+	T.expect_signed(s, 0);
+	T.expect_signed(list.size(), 2);
 
 	s = list.remove(b0);
-	assert(s == 0);
-	assert(list.size() == 1);
+	T.expect_signed(s, 0);
+	T.expect_signed(list.size(), 1);
 
 	s = list.remove(b2);
-	assert(s == 0);
-	assert(list.size() == 0);
+	T.expect_signed(s, 0);
+	T.expect_signed(list.size(), 0);
 
 	// the end.
 	delete b2;
@@ -304,22 +307,22 @@ void test_sort()
 	Buffer* b2 = new Buffer();
 	b2->copy_raw(STR_TEST_2);
 
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_tail(b2);
 	list.sort(Buffer::CMP);
-	assert(list.size() == 1);
-	assert(strcmp(EXP_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 1);
+	T.expect_string(EXP_2, list.chars());
 
 	list.push_tail(b1);
 	list.sort(Buffer::CMP);
-	assert(list.size() == 2);
-	assert(strcmp(EXP_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 2);
+	T.expect_string(EXP_1_2, list.chars());
 
 	list.push_tail(b0);
 	list.sort(Buffer::CMP);
-	assert(list.size() == 3);
-	assert(strcmp(EXP_0_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_0_1_2, list.chars());
 
 	list.reset();
 }
@@ -338,30 +341,30 @@ void test_search()
 	Buffer* b2 = new Buffer();
 	b2->copy_raw(STR_TEST_2);
 
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	node_found = list.node_search(b1, NULL);
-	assert(node_found == NULL);
+	T.expect_ptr(node_found, NULL);
 
 	list.push_tail(b0);
 	list.push_tail(b1);
 	list.push_tail(b2);
 
 	node_found = list.node_search(b1, NULL);
-	assert(node_found != NULL);
-	assert(node_found == list.node_at(1));
+	T.expect_ptr(node_found, NULL, vos::IS_NOT_EQUAL);
+	T.expect_ptr(node_found, list.node_at(1));
 
 	node_found = list.node_search(b2, Buffer::CMP);
-	assert(node_found != NULL);
-	assert(node_found == list.node_at(2));
+	T.expect_ptr(node_found, NULL, vos::IS_NOT_EQUAL);
+	T.expect_ptr(node_found, list.node_at(2));
 
 	bdel = (Buffer*) list.node_remove_unsafe(node_found);
 
-	assert(list.size() == 2);
-	assert(bdel == b2);
+	T.expect_signed(list.size(), 2);
+	T.expect_ptr(bdel, b2);
 
 	node_found = list.node_search(b2, Buffer::CMP);
-	assert(node_found == NULL);
+	T.expect_ptr(node_found, NULL);
 
 	list.reset();
 
@@ -379,38 +382,38 @@ void test_push_head_sorted_asc()
 	Buffer* b2 = new Buffer();
 	b2->copy_raw(STR_TEST_2);
 
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_head_sorted(b2);
 	list.push_head_sorted(b1);
 	list.push_head_sorted(b0);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_0_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_0_1_2, list.chars());
 
 	list.pop_tail();
 	list.pop_tail();
 	list.pop_tail();
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_head_sorted(b0);
 	list.push_head_sorted(b1);
 	list.push_head_sorted(b2);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_0_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_0_1_2, list.chars());
 
 	list.pop_tail();
 	list.pop_tail();
 	list.pop_tail();
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_head_sorted(b2);
 	list.push_head_sorted(b0);
 	list.push_head_sorted(b1);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_0_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_0_1_2, list.chars());
 
 	list.reset();
 }
@@ -426,38 +429,38 @@ void test_push_head_sorted_desc()
 	Buffer* b2 = new Buffer();
 	b2->copy_raw(STR_TEST_2);
 
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_head_sorted(b2, 0);
 	list.push_head_sorted(b1, 0);
 	list.push_head_sorted(b0, 0);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_2_1_0, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_2_1_0, list.chars());
 
 	list.pop_tail();
 	list.pop_tail();
 	list.pop_tail();
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_head_sorted(b0, 0);
 	list.push_head_sorted(b1, 0);
 	list.push_head_sorted(b2, 0);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_2_1_0, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_2_1_0, list.chars());
 
 	list.pop_tail();
 	list.pop_tail();
 	list.pop_tail();
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_head_sorted(b0, 0);
 	list.push_head_sorted(b2, 0);
 	list.push_head_sorted(b1, 0);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_2_1_0, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_2_1_0, list.chars());
 
 	list.reset();
 }
@@ -473,38 +476,38 @@ void test_push_tail_sorted_asc()
 	Buffer* b2 = new Buffer();
 	b2->copy_raw(STR_TEST_2);
 
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_tail_sorted(b2);
 	list.push_tail_sorted(b1);
 	list.push_tail_sorted(b0);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_0_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_0_1_2, list.chars());
 
 	list.pop_tail();
 	list.pop_tail();
 	list.pop_tail();
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_tail_sorted(b0);
 	list.push_tail_sorted(b1);
 	list.push_tail_sorted(b2);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_0_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_0_1_2, list.chars());
 
 	list.pop_tail();
 	list.pop_tail();
 	list.pop_tail();
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_tail_sorted(b0);
 	list.push_tail_sorted(b2);
 	list.push_tail_sorted(b1);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_0_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_0_1_2, list.chars());
 
 	list.reset();
 }
@@ -520,38 +523,38 @@ void test_push_tail_sorted_desc()
 	Buffer* b2 = new Buffer();
 	b2->copy_raw(STR_TEST_2);
 
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_tail_sorted(b0, 0);
 	list.push_tail_sorted(b1, 0);
 	list.push_tail_sorted(b2, 0);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_2_1_0, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_2_1_0, list.chars());
 
 	list.pop_tail();
 	list.pop_tail();
 	list.pop_tail();
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_tail_sorted(b2, 0);
 	list.push_tail_sorted(b1, 0);
 	list.push_tail_sorted(b0, 0);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_2_1_0, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_2_1_0, list.chars());
 
 	list.pop_tail();
 	list.pop_tail();
 	list.pop_tail();
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_tail_sorted(b0, 0);
 	list.push_tail_sorted(b2, 0);
 	list.push_tail_sorted(b1, 0);
 
-	assert(list.size() == 3);
-	assert(strcmp(EXP_2_1_0, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_2_1_0, list.chars());
 
 	list.reset();
 }
@@ -569,51 +572,51 @@ void test_detach()
 	Buffer* b2 = new Buffer();
 	b2->copy_raw(STR_TEST_2);
 
-	assert(list.size() == 0);
+	T.expect_signed(list.size(), 0);
 
 	list.push_tail(b0);
 	list.push_tail(b1);
 	list.push_tail(b2);
-	assert(list.size() == 3);
+	T.expect_signed(list.size(), 3);
 
 	node = list.node_at(0);
 	list.detach(node);
-	assert(list.size() == 2);
-	assert(strcmp(EXP_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 2);
+	T.expect_string(EXP_1_2, list.chars());
 
 	node->set_content(NULL);
 	delete node;
 
 	list.push_head(b0);
-	assert(list.size() == 3);
-	assert(strcmp(EXP_0_1_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 3);
+	T.expect_string(EXP_0_1_2, list.chars());
 
 	node = list.node_at(1);
 	list.detach(node);
-	assert(list.size() == 2);
-	assert(strcmp(EXP_0_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 2);
+	T.expect_string(EXP_0_2, list.chars());
 
 	delete node;
 
 	node = list.node_at(0);
 	list.detach(node);
-	assert(list.size() == 1);
-	assert(strcmp(EXP_2, list.chars()) == 0);
+	T.expect_signed(list.size(), 1);
+	T.expect_string(EXP_2, list.chars());
 
 	delete node;
 
 	node = list.node_at(0);
 	list.detach(node);
-	assert(list.size() == 0);
-	assert(list.chars() == 0);
+	T.expect_signed(list.size(), 0);
+	T.expect_ptr(list.chars(), 0);
 
 	delete node;
 }
 
 int main()
 {
-	assert(list.size() == 0);
-	assert(NULL == list.chars());
+	T.expect_signed(list.size(), 0);
+	T.expect_ptr(NULL, list.chars());
 
 	test_at_0();
 
